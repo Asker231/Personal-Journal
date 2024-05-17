cbb403b413c0de64e4351a617c76be9b2bbedead
Asker <kotsevasker0@gmail.com>
2024-05-17 18:34:30 +0300

    add textarea

--- src/layout/Body/Body.jsx
+++ src/layout/Body/Body.jsx
@@ -3,13 +3,28 @@ import Button from '../../components/Button/Button';
 import style from './body.module.css';
 import cn from 'classnames'
 import arh from '../../assets/Archive.svg'
+import Calendar from '../../assets/Calendar.svg'
+import Folder from '../../assets/Folder.svg'
 import CardButton from '../../components/CardButton/CardButton.jsx'
 
 
 function Body() {
+  const[focusarea,setFocusArea] = useState(false)
   const[focus,setFocus] = useState(false)
+
+  let date = Intl.DateTimeFormat('ru-RU').format(new Date())
+
+  const FocuArea = ()=>{
+    setFocusArea(focussrea => !focussrea)
+  }
+
+  const BlurArea = () =>{
+    setFocusArea(focussrea=>!focussrea)
+  }
+
   const submitForm = (e)=>{
     e.preventDefault()
+    console.log(e);
   }
   return (
     <div className={style.body}>
@@ -17,6 +32,7 @@ function Body() {
       <div className={style.topHstack}>
         <input
         onFocus={()=>setFocus(focus => !focus)}
+        onBlur={()=>setFocus(focus=>!focus)}
         name='title' 
         className={cn(
           style["title"],
@@ -31,6 +47,30 @@ function Body() {
       <img className={style.arh} src={arh} alt="archive" />
       </CardButton>
       </div>  
+       <div className={style.vstack}>
+       <div className={style.date}>
+          <div className={style.hstackOne}>
+          <img src={Calendar} alt="Calendar"/>
+          <span>Дата</span>
+          </div>
+          <span>{date}</span>
+         </div>
+        <Divider/>
+         <div className={style.map}>
+          <div className={style.hstackmap}>
+          <img src={Folder} alt="Folder"/>
+          <span>Метки</span>
+          </div>
+          <span>Спорт</span>
+         </div>
+         <Divider/>
+        <textarea  
+           onFocus={FocuArea}
+           onBlur={BlurArea} 
+           className={cn(style["textarea"], focusarea && style["textActive"])} 
+           rows={25} 
+           cols={30}/>
+       </div>
         <Button text={"Сохранить"}/>
       </form>
     </div>
